c51b33490a70262da0635c61da0fb43857776a17
geonhui160 <jcv31312@naver.com>
2024-11-01 15:52:32 +0900

    feat : first commit - UMC_study_week5_create table with jpa

---
+++ spring_week5/src/test/java/umc/spring_week5/SpringWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.spring_week5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
